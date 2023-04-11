6e2ad5df6691b82ee0b074a33d6b2b4c97ad6b7f
Irianto <irianto223@gmail.com>
2023-04-11 18:31:24 +0700

    remove un-used import

--- src/main/java/com/iriantokbarek/springbootmongo/task/TaskService.java
+++ src/main/java/com/iriantokbarek/springbootmongo/task/TaskService.java
@@ -1,6 +1,5 @@
 package com.iriantokbarek.springbootmongo.task;
 
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -25,8 +24,6 @@ public class TaskService {
     public Task getTaskById(String taskId) {
         return repository
                 .findById(taskId)
-                .orElseThrow(() -> {
-                    return new ResponseStatusException(HttpStatus.NOT_FOUND, "Task not found");
-                });
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Task not found"));
     }
 }
